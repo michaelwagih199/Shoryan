da882b9a1ba8d7f93ece1b8304982b12e5dd14f7
michaelwagih199 <michaelwagih199@gmail.com>
2019-05-13 13:38:26 +0300

    13/5

--- app/src/main/java/com/example/projectui/SignUpAccount.java
+++ app/src/main/java/com/example/projectui/SignUpAccount.java
@@ -3,30 +3,44 @@ package com.example.projectui;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class SignUpAccount extends AppCompatActivity {
 
     //defining view objects
     private EditText editTextEmail;
-    private EditText editTextPassword;
+    private EditText editTextPassword , et_comfirmPass;
     private Button buttonSignup;
     private ProgressDialog progressDialog;
 
     FirebaseAuth firebaseAuth;
+    GoogleSignInClient mGoogleSignInClient;
+    SignInButton signInButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,10 +54,17 @@ public class SignUpAccount extends AppCompatActivity {
         //initializing views
         editTextEmail = (EditText) findViewById(R.id.editTextEmail);
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        et_comfirmPass =(EditText)findViewById(R.id.et_comfirmPass);
+        signInButton = (SignInButton) findViewById(R.id.googelSignIn);
 
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this,gso);
 
         buttonSignup = (Button) findViewById(R.id.buttonSignup);
-
         progressDialog = new ProgressDialog(this);
 
         buttonSignup.setOnClickListener(new View.OnClickListener() {
@@ -53,14 +74,76 @@ public class SignUpAccount extends AppCompatActivity {
             }
         });
 
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent,101);
+
+            }
+        });
+
+
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, 101);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == 101) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("tag", "Google sign in failed", e);
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            Intent mainIntent = new Intent(SignUpAccount.this, HomeActivity.class);
+                            startActivity(mainIntent);
+                            finish();
+                            Toast.makeText(SignUpAccount.this,"Successfully registered",Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            Toast.makeText(SignUpAccount.this,"Registration Error",Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
 
     }
 
     private void registerUser(){
 
         //getting email and password from edit texts
-        String email = editTextEmail.getText().toString().trim();
-        String password  = editTextPassword.getText().toString().trim();
+        String email  =  editTextEmail.getText().toString().trim();
+        String password  =  editTextPassword.getText().toString().trim();
+        String comfirmPassword  = et_comfirmPass.getText().toString().trim();
 
         //checking if email and passwords are empty
         if(TextUtils.isEmpty(email)){
@@ -70,6 +153,14 @@ public class SignUpAccount extends AppCompatActivity {
 
         if(TextUtils.isEmpty(password)){
             Toast.makeText(this,"Please enter password",Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (! comfirmPassword.equals(password)){
+            Toast.makeText(this,"password not match ",Toast.LENGTH_LONG).show();
+            return;
+        }
+        if(TextUtils.isEmpty(comfirmPassword)){
+            Toast.makeText(this," Please enter comfirm password ",Toast.LENGTH_LONG).show();
             return;
         }
 
@@ -88,7 +179,7 @@ public class SignUpAccount extends AppCompatActivity {
                         if(task.isSuccessful()){
                             //display some message here
                             Toast.makeText(SignUpAccount.this,"Successfully registered",Toast.LENGTH_LONG).show();
-                            Intent intent = new Intent(getApplicationContext(),Profile.class);
+                            Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
                             startActivity(intent);
 
                         }else{
